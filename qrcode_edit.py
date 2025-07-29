3de76dc684fa36a0026794d37435906c84348da0
usman-the-bloodborn-0 <usmankhan16122006@gmail.com>
2025-07-29 23:29:10 +0500

    Practice for generating QR code

---
+++ qrcode_edit.py
@@ -0,0 +1,7 @@
+#Generate QR code using python
+
+import qrcode
+data = "www.linkedin.com/in/usman-khan-735944353"
+qr = qrcode.make(data)
+qr.save("linkedin_qr.png")
+print("QR code generated successfully!!")
