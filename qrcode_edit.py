c5c7ac4d505a15f1d90a6b83550038d09ba977c0
usman-the-bloodborn-0 <usmankhan16122006@gmail.com>
2025-08-03 20:26:46 +0500

    New Updates

--- qrcode_edit.py
+++ qrcode_edit.py
@@ -5,3 +5,4 @@ data = "www.linkedin.com/in/usman-khan-735944353"
 qr = qrcode.make(data)
 qr.save("linkedin_qr.png")
 print("QR code generated successfully!!")
+print("QR code saved as linkedin_qr.png")
